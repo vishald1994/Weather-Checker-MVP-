f56e33e88320bf84c2d37c6f2478e43f2b22be88
Vishal Dave <49125707+vishald1994@users.noreply.github.com>
2019-09-17 10:13:11 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const searchCity = document.getElementById('searchCity');
+const appID = "501859519a7e0d160d90e4719d76e3c6";
+const weatherParam = 'weather';
+
+searchCity.addEventListener('change', () => {
+  const searchValue = searchCity.value;
+  getTodaysWeather(searchValue);
+})
+
+getTodaysWeather();
+
+// Expects a parameter!!!
+function getTodaysWeather(city = ""){
+  // Only if value isn't present
+  // if(!city){
+  //   city = "";
+  // }
+  // // Ternary operator
+  // city ? "" : city;
+  fetch(`https://api.openweathermap.org/data/2.5/${weatherParam}?q=${city}&APPID=${appID}&units=imperial&lang=en`)
+    .then((response) => response.json())
+    .then((weatherData) =>  {
+      displayWeather(weatherData)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+
+
+function displayWeather(weatherData){
+  // const main = weatherData.main;
+  //const weather = weatherData.weather;
+  const { main, weather } = weatherData;
+  const weatherInfoElement = document.getElementById('weatherInfo');
+  let weatherInfo = `
+    <img src="https://openweathermap.org/img/w/${weather[0].icon}.png" />
+    <p> ${weather[0].description} </p>
+    <p> ${main.temp} ℃ </p>
+  `;
+  weatherInfoElement.innerHTML = weatherInfo;
+}
+
