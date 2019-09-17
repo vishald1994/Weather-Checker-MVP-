b65e2ac15967bead83236403f9d687b929b94232
Vishal Dave <49125707+vishald1994@users.noreply.github.com>
2019-09-17 11:54:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 function mainFunction() {
-   const searchCity = document.getElementById('searchCity');
-const appID = "501859519a7e0d160d90e4719d76e3c6";
-const weatherParam = 'weather';
-
-searchCity.addEventListener('change', () => {
-  const searchValue = searchCity.value;
+   const searchCity =            searchCity.value;
   getTodaysWeather(searchValue);
 })
 
@@ -13,46 +8,54 @@ getTodaysWeather();
 }
 
 $(mainFunction)
-const searchCity = document.getElementById('searchCity');
-const appID = "501859519a7e0d160d90e4719d76e3c6";
-const weatherParam = 'weather';
+const appKey = "501859519a7e0d160d90e4719d76e3c6";
+let searchButton = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city-name");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temp");
+let humidity = document.getElementById("humidity-div");
 
-searchCity.addEventListener('change', () => {
-  const searchValue = searchCity.value;
-  getTodaysWeather(searchValue);
-})
+searchButton.addEventListener("click", findWeatherDetails);
+searchInput.addEventListener("keyup", enterPressed);
 
-getTodaysWeather();
-
-// Expects a parameter!!!
-function getTodaysWeather(city = ""){
-  // Only if value isn't present
-  // if(!city){
-  //   city = "";
-  // }
-  // // Ternary operator
-  // city ? "" : city;
-  fetch(`https://api.openweathermap.org/data/2.5/${weatherParam}?q=${city}&APPID=${appID}&units=imperial&lang=en`)
-    .then((response) => response.json())
-    .then((weatherData) =>  {
-      displayWeather(weatherData)
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+function enterPressed(event) {
+  if (event.key === "Enter") {
+    findWeatherDetails();
+  }
 }
 
-
-function displayWeather(weatherData){
-  // const main = weatherData.main;
-  //const weather = weatherData.weather;
-  const { main, weather } = weatherData;
-  const weatherInfoElement = document.getElementById('weatherInfo');
-  let weatherInfo = `
-    <img src="https://openweathermap.org/img/w/${weather[0].icon}.png" />
-    <p> ${weather[0].description} </p>
-    <p> ${main.temp} ℃ </p>
-  `;
-  weatherInfoElement.innerHTML = weatherInfo;
+function cToF(celsius){
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+    return cToFahr;
 }
 
+function findWeatherDetails() {
+  if (searchInput.value === "") {
+  }
+  else {
+    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
+   httpRequestAsync(searchLink, theResponse);
+  }
+ }
+
+function theResponse(response) {
+  let jsonObject = JSON.parse(response);
+  cityName.innerHTML = jsonObject.name;
+  icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+  temperature.innerHTML = parseInt(cToF(jsonObject.main.temp - 273)) + "°";
+  humidity.innerHTML = jsonObject.main.humidity + "%";
+}
+
+function httpRequestAsync(url, callback)
+{
+  console.log("hello");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true); // true for asynchronous
+    httpRequest.send();
+}
